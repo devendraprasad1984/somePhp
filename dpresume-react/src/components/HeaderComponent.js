f9d2850950b2c0836c894dd6ef9f1dd47d912d82
devendraprasad1984@gmail.com <devendraprasad1984@gmail.com>
2019-04-07 07:06:17 +0530

    update

--- dpresume-react/src/components/HeaderComponent.js
+++ dpresume-react/src/components/HeaderComponent.js
@@ -1,12 +1,14 @@
-import React,{Component} from 'react'
+import React, {Component} from 'react'
 import '../styles/HeaderComponent.css'
+import LinksComponent from "./LinksComponent"
 
-export default class HeaderComponent extends Component{
-    render(){
-        return(
+export default class HeaderComponent extends Component {
+    render() {
+        return (
             <div>
                 <div className="h_left">
                     Devendra Prasad - Tech Lead Developer
+                    <LinksComponent/>
                 </div>
                 <div className="h_right">
                     Email: devendraprasad1984@gmail.com<br/>
---
+++ dpresume-react/src/components/LinksComponent.js
@@ -0,0 +1,21 @@
+import React, {Component} from 'react'
+
+export default class LinksComponent extends Component {
+    render() {
+        return (
+            <span>
+                <a href="http://dpresume.com/older/docs/Devendra_Prasad_sidebar.pdf" rel="noopener noreferrer"
+                   target="_blank"><i className="fas fa-download" aria-hidden="true"></i></a>
+                <a rel="noopener noreferrer" target="_blank"
+                   href="http://dpresume.com/older/docs/pythonTrainingKIT.pdf"><i className="fab fa-python"
+                                                                                  aria-hidden="true"></i></a>
+                <a rel="noopener noreferrer" target="_blank"
+                   href="http://dpresume.com/older/docs/aws-symbi-project.pdf"><i className="fas fa-cloud"
+                                                                                  aria-hidden="true"></i></a>
+                <a rel="noopener noreferrer" target="_blank"
+                   href="http://dpresume.com/older/docs/aws-dev-associate-notes.pdf"><i
+                    className="fab fa-aws"></i></a>
+            </span>
+        )
+    }
+}
