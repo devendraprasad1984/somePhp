9d9edc720c38fcd938cb4a02aa1351bc49ca393e
devendraprasad1984@gmail.com <devendraprasad1984@gmail.com>
2019-12-05 22:21:19 +0530

    Update info.txt, d7a7cea160667b981d6978cbf7d682f8b4e73a25, and 3 more files...

--- kaathi.com/js/index.js
+++ kaathi.com/js/index.js
@@ -1,11 +1,11 @@
-var rs = '₹';
-var product_img_base = 'imgs/products/';
-var qty = 0;
-var priceTag = "#priceTag";
-var v_products = {
+let rs = '₹';
+let product_img_base = 'imgs/products/';
+let qty = 0;
+let priceTag = "#priceTag";
+let v_products = {
     1: {
         code: 'prod1',
-        desc: 'desc1',
+        desc: 'desc1desc1desc1desc1desc1desc1desc1desc1desc1desc1desc1desc1desc1desc1desc1desc1desc1desc1desc1desc1desc1',
         images: ['1.jpeg'],
         price: 10,
         discount: 0,
@@ -52,10 +52,10 @@ var v_products = {
         amzlink: 'https://www.amazon.in/Mammon-Womens-Leather-Handbag-3L-bib-Cream/dp/B07XKNS6FF/ref=lp_19079038031_1_1?s=shoes&ie=UTF8&qid=1574514565&sr=1-1'
     }
 }
-var mainContainer = '#id_div_container';
-var selectedProduct = {}
-var rightContainer="#id_div_right_container";
-var cartObj = {}
+let mainContainer = '#id_div_container';
+let selectedProduct = {}
+let rightContainer="#id_div_right_container";
+let cartObj = {}
 let checkOutPayment=0;
 
 $(document).ready(function () {
@@ -78,50 +78,50 @@ $(document).ready(function () {
     displayProducts();
 });
 
-var displayProducts = () => {
+let displayProducts = () => {
     checkOutPayment=0;
     $(mainContainer).empty();
     for (x in v_products) {
-        v_product = v_products[x];
-        pname = v_product.code;
-        desc = v_product.desc;
-        images = v_product.images;
-        price = v_product.price;
-        amzLink = v_product.amzlink;
+        let v_product = v_products[x];
+        let pname = v_product.code;
+        let desc = v_product.desc;
+        let images = v_product.images;
+        let price = v_product.price;
+        let amzLink = v_product.amzlink;
         v_product["qty"] = 0;
 
-        elm1 = '<div>' +
+        let elm1 = '<div>' +
             '<h2>' + pname + ' ' +
             '<span class="btn btn-success" onclick="handleProductClick(\'' + x + '\',\'' + pname + '\',1)"><i class="fa fa-lg fa-plus"></i></span>' +
             ' <span class="btn btn-danger" onclick="handleProductClick(\'' + x + '\',\'' + pname + '\',-1)"><i class="fa fa-lg fa-minus"></i></span>' +
             '</h2>' +
             '</div>';
-        elm2 = '<div><b id="id_prod_desc_"' + x + '>' + desc + '</b></div>';
-        elm3 = '<div><span id="id_img_desc_"' + x + ' class="productImages">' + display_product_images(x, images) + '</span></div>';
+        let elm2 = '<div><b id="id_prod_desc_"' + x + '>' + desc + '</b></div>';
+        let elm3 = '<div><span id="id_img_desc_"' + x + ' class="productImages">' + display_product_images(x, images) + '</span></div>';
         // elm3_1 = '<div><span id="id_basePrice_"' + x + ' >Base Price: ' +rs+price + '</span></div>';
-        elm4 = '<div id="priceTag_' + x + '" class="priceline color1">' + getPriceLine(v_product, qty) + '</div>';
-        elm5 = '<div class="link_logo">' +
+        let elm4 = '<div id="priceTag_' + x + '" class="priceline color1">' + getPriceLine(v_product, qty) + '</div>';
+        let elm5 = '<div class="link_logo">' +
             ' <a target="_blank" id="id_amazon_"' + x + ' href="' + amzLink + '">amazon</a>' +
             '</div>';
-        shtml = '<div class="col-lg-12 cenAlign">' + elm1 + elm4 + elm2 + elm3 + elm5 + '</div><br/>';
+        let shtml = '<div class="col-lg-12 cenAlign product_box">' + elm1 + elm4 + elm2 + elm3 + elm5 + '</div><br/>';
 
         $(mainContainer).append(shtml);
 
         initCart();
-        closeCart();
+        closeRightPanel();
     }
 }
 
-var getCalci = (prod, qty) => {
-    discount = prod.discount;
-    price = prod.price * qty;
-    finalAmount = Math.round(price - price * discount / 100, 0);
-    savedAmount = price - finalAmount;
+let getCalci = (prod, qty) => {
+    let discount = prod.discount;
+    let price = prod.price * qty;
+    let finalAmount = Math.round(price - price * discount / 100, 0);
+    let savedAmount = price - finalAmount;
     let objCalc = {price, discount, finalAmount, savedAmount, qty};
     prod.finalAmount = finalAmount;
     return objCalc;
 }
-var getPriceLine = (prod, qty) => {
+let getPriceLine = (prod, qty) => {
     let x = getCalci(prod, qty);
     let shtml = '<div><span>' + rs + prod.price + '*' + qty + '</span> ' +
         '<span> - ' + rs + x.savedAmount + '(' + x.discount + '%)</span> ' +
@@ -130,7 +130,7 @@ var getPriceLine = (prod, qty) => {
     return shtml;
 }
 
-var display_product_images = (pid, imgs) => {
+let display_product_images = (pid, imgs) => {
     var sImages = '<div class="row">';
     for (i in imgs) {
         sImages += '<img class="col-lg-4" src="' + product_img_base + pid + '/' + imgs[i] + '" alt="' + imgs[i] + '" />';
@@ -139,8 +139,8 @@ var display_product_images = (pid, imgs) => {
     return sImages
 }
 
-var add2cart = (xid) => {
-    prods = getProductByCode(xid);
+let add2cart = (xid) => {
+    let prods = getProductByCode(xid);
     if (prods.qty === undefined || prods.qty <= 0 || prods.finalAmount <= 0) {
         toastr.error("choose min 1 qty");
         return;
@@ -158,9 +158,9 @@ var add2cart = (xid) => {
     }
 }
 
-var initCart=()=>{
+let initCart=()=>{
     $(rightContainer).empty();
-    let elm0="<h2>Cart has 0 item of <span class='badge badge-light text-danger'>"+rs+0+"</span> <span style='cursor: pointer;' class='btn btn-dark' onclick='closeCart()'>Close</span></h2>";
+    let elm0="<h2>Cart has 0 item of <span class='badge badge-light text-danger'>"+rs+0+"</span> <span style='cursor: pointer;' class='btn btn-dark' onclick='closeRightPanel()'>Close</span></h2>";
     let elm0_1="<span class='btn btn-light font-weight-bold disabled'>Pay</span>";
     let elm0_2=" <span class='btn btn-light font-weight-bold disabled' onclick='clearAll()'>Clear</span>";
     let elm0_4="<h4>Thanks for using kaathi.com</h4>";
@@ -173,45 +173,44 @@ var displayCart = () => {
     let Amount=0;
     for (o in cartObj) {
         let prod = cartObj[o];
-        let elm1="<div class=\"card\" style=\"width: 100%;\">";
-        let elm2="<div class=\"card-body\">";
-        let elm3='<h5 class="card-title">'+prod.code+' <a href="#" class="btn btn-danger" onclick="removeFromCart(\''+prod.code+'\')">X</a></h5>';
-        let elm4="<p class=\"card-text\">"+prod.desc+prod.calci+"</p>";
-        let elm5="</div></div>";
-        $(rightContainer).append(elm1+elm2+elm3+elm4);
+        let elm1='<div class="xcard">';
+        let elm3='<h3>'+prod.code+' <a href="#" class="btn btn-danger" onclick="removeFromCart(\''+prod.code+'\')">X</a></h3>';
+        let elm4='<span>'+prod.desc+prod.calci+'</span>';
+        let elm5='</div>';
+        $(rightContainer).append(elm1+elm3+elm4);
 
         count+=1;
         Amount+=prod.finalAmount;
     }
-    let elm0="<h2>Cart has "+count+" item of <span class='badge badge-light text-danger'>"+rs+Amount+"</span> <span style='cursor: pointer;' class='btn btn-dark' onclick='closeCart()'>Close</span></h2>";
+    let elm0="<h2>Cart has "+count+" item of <span class='badge badge-light text-danger'>"+rs+Amount+"</span> <span style='cursor: pointer;' class='btn btn-dark' onclick='closeRightPanel()'>Close</span></h2>";
     let elm0_1="<a target='_blank' href='#' class='btn btn-light font-weight-bold'>Pay</a>";
     let elm0_2=" <span class='btn btn-light font-weight-bold' onclick='clearAll()'>Clear</span>";
     let elm0_4="<h4>Thanks for using kaathi.com</h4>";
     $(rightContainer).prepend(elm0+elm0_1+elm0_2+elm0_4);
     $(".cart_bg div i.fa.fa-2x.fa-check-square.click").remove();
-    showCart();
+    showRightPanel();
     checkOutPayment=Amount; //checkout amount that we may want to charge
 }
 
-var showCart=()=>{
+let showRightPanel=()=>{
     $(rightContainer).css({display:'block'});
 }
-var closeCart=()=>{
+let closeRightPanel=()=>{
     $(rightContainer).css({display:'none'});
 }
 
-var removeFromCart=(pid)=>{
+let removeFromCart=(pid)=>{
     delete cartObj[pid];
     displayCart();
 }
 
-var clearAll=()=>{
+let clearAll=()=>{
     checkOutPayment=0;
     $(rightContainer).empty();
     displayProducts();
 }
 
-var getProductByCode = (code) => {
+let getProductByCode = (code) => {
     let pr = {}
     for (i in v_products) {
         if (v_products[i].code == code) {
@@ -222,7 +221,7 @@ var getProductByCode = (code) => {
     return pr;
 }
 
-var handleProductClick = (pid, pname, qty) => {
+let handleProductClick = (pid, pname, qty) => {
     selectedProduct = v_products[pid];
     if (selectedProduct.qty == undefined) selectedProduct.qty = 0;
     selectedProduct.qty += qty;
